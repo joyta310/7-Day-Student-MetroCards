b573e3f519e047d339e4170b6ea3c2eb7d3ff7b1
joyta310 <58852895+joyta310@users.noreply.github.com>
2020-02-04 09:00:16 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,47 @@
+$(".Monday").click(function() {
+    $(".Monday").fadeOut();
+    $(".Tuesday").fadeIn();
+});
+$(".Tuesday").click(function() {
+    $(".Tuesday").fadeOut();
+    $(".Wednesday").fadeIn();
+});
+$(".Wednesday").click(function() {
+    $(".Wednesday").fadeOut();
+    $(".Thursday").fadeIn();
+});
+$(".Thursday").click(function() {
+    $(".Thursday").fadeOut();
+    $(".Friday").fadeIn();
+});
+$(".Friday").click(function() {
+    $(".Friday").fadeOut();
+    $(".Sat").fadeIn();
+});
+$(".Sat").click(function() {
+    $(".Sat").fadeOut();
+    $(".Sun").fadeIn();
+});
+$(".Sun").click(function() {
+    $(".Sun").fadeOut();
+    $(".Info").fadeIn();
+});
+$(".Info").click(function() {
+    $(".Info").fadeOut();
+    $(".pet").fadeIn();
+});
+$("button").click(function() {
+    $(".pet").fadeOut();
+    $(".start").fadeOut();
+    $(".fin").fadeIn();
+});
+$(".exit").click(function() {
+    $(".fin").fadeOut();
+    $(".start").fadeIn();
+    $(".Monday").fadeIn();
+});
+
+
+
+
+
